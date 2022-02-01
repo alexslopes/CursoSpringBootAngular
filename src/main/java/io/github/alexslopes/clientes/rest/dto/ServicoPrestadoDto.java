51e5ee4b056c2b306c09e1904d96e15ce0f887ab
alexs <alexstlopes@gmail.com>
2022-01-31 22:08:32 -0300

    86. Mensagem de feedback no formulario de Serviços Prestados

--- src/main/java/io/github/alexslopes/clientes/rest/dto/ServicoPrestadoDto.java
+++ src/main/java/io/github/alexslopes/clientes/rest/dto/ServicoPrestadoDto.java
@@ -3,12 +3,22 @@ package io.github.alexslopes.clientes.rest.dto;
 import lombok.Data;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 
 @Data
 @RequestMapping("/api/servicos-prestados")
 public class ServicoPrestadoDto {
+    @NotEmpty(message = "{campo.descricao.obrigatorio}")
     private String descricao;
+
+    @NotEmpty(message = "{campo.preco.obrigatorio}")
     private String preco;
+
+    @NotEmpty(message = "{campo.data.obrigatorio}")
     private String data;
+
+    @NotNull(message = "{campo.cliente.obrigatorio}")
     private Integer idCliente;
 }
