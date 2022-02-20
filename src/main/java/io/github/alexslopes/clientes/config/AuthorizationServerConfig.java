c842e95ff7ace994ed201b99134b72c23b80d961
alexs <alexstlopes@gmail.com>
2022-02-20 20:56:33 -0300

    101. Configurando o Authorization Server

--- src/main/java/io/github/alexslopes/clientes/config/AuthorizationServerConfig.java
+++ src/main/java/io/github/alexslopes/clientes/config/AuthorizationServerConfig.java
@@ -1,4 +1,44 @@
 package io.github.alexslopes.clientes.config;
 
-public class AuthorizationServerConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Bean
+    public TokenStore tokenStore(){
+        return new InMemoryTokenStore();//Gera token em memória
+    }
+
+    @Override//Configura os endpoints
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints
+                .tokenStore(tokenStore())//Define a origem dos tokens
+                .authenticationManager(authenticationManager);
+    }
+
+    @Override//Configura os acessos dos clients (as aplicações clientes)
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        //Foi cadastrado nesse exemplo apenas uma aplicação
+        clients
+            .inMemory()
+            .withClient("my-angular-app")
+            .secret("@321")//Em produção usar um secret mais complexo
+            .scopes("read", "write")//define permissao para ler e escrever dados
+            .authorizedGrantTypes("password")//Grant types que são perrmitidos
+            .accessTokenValiditySeconds(1800);//Define a duração do token
+    }
 }
